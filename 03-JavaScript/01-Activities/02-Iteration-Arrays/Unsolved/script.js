088805c3b301c96c877a0049c9d1dbd418c2eba2
Dana Bottoni <danabottoni@gmail.com>
2021-06-07 19:12:12 -0400

    update repo

---
+++ 03-JavaScript/01-Activities/02-Iteration-Arrays/Unsolved/script.js
@@ -0,0 +1,15 @@
+      // Array of zoo animals.
+      var zooAnimals = ['Zebra', 'Rhino', 'Giraffe', 'Owl'];
+
+      // TODO: Refactor the following code to print all of the animals using a for-loop.
+      console.log(zooAnimals[0]);
+      console.log(zooAnimals[1]);
+      console.log(zooAnimals[2]);
+      console.log(zooAnimals[3]);
+      console.log(zooAnimals.length);
+
+
+      for (var i = 0; i < zooAnimals.length; i++ )  {
+
+        console.log(zooAnimals[i]);
+      }
