d1970f8206660182520c5b3243005f56195a4875
Dana Bottoni <danabottoni@gmail.com>
2021-06-08 13:30:19 -0400

    refactor

--- 04-Web-APIs/01-Activities/01-DOM-manipulation/Unsolved/assets/js/script.js
+++ 04-Web-APIs/01-Activities/01-DOM-manipulation/Unsolved/assets/js/script.js
@@ -4,7 +4,14 @@ var body = document.body;
 
 // TODO: Add a centered h1
 
+var h1El = document.createElement('h1');
+
+//document.getElementById("main").setAttribute()
+//document.getElementById("main").textContent = "welcome"
+
 // TODO: Add a centered h2
+// variable.setAttribute
+// h1El.setAttribute('style', 'margin:auto; width:50%; text-alignment:center;' etc )
 
 // TODO: Add a centered image with a centered caption under it
 
